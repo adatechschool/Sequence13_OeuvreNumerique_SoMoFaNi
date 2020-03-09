d819d9879cb8d4212d03ecfcab60c4d39f5aeb7a
Mohamed CHACHOUR <Mochacho42@users.noreply.github.com>
2020-03-09 15:51:23 +0100

    Créer la terre

---
+++ Earth_Moon_Sun/earth.py
@@ -0,0 +1,9 @@
+class EarthObject():
+    def __init__(self):
+        return
+
+    def earthDraw(self):
+        surface = 15
+        ellipse(width/2, height/2, width/(surface*1.777), height/surface)
+        fill(53, 210, 249)
+        return
